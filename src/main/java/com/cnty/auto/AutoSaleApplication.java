27658cc58ef7f51f30796c3296e3ca38a7ce74e4
mystic_cg <215104920@qq.com>
2017-11-24 13:58:01 +0800

    init project

---
+++ src/main/java/com/cnty/auto/AutoSaleApplication.java
@@ -0,0 +1,21 @@
+package com.cnty.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: mystic
+ * @date: 2017/11/24 13:54
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+@SpringBootApplication
+public class AutoSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSaleApplication.class, args);
+	}
+}
