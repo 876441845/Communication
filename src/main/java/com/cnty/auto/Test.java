f8e7f8660341f8f9e52714885d94f86dfce75441
pokerface <876441845@qq.com>
2017-11-24 15:13:05 +0800

    ce

---
+++ src/main/java/com/cnty/auto/Test.java
@@ -0,0 +1,13 @@
+package com.cnty.auto;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: pokerface
+ * @date: 2017/11/24 0024 15:11
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public class Test {
+}
