30d39d27fd948aa3828739ec5b2c43a35a1f8a92
mystic_cg <215104920@qq.com>
2017-11-29 10:23:06 +0800

    Server class has changed

--- src/main/java/com/cnty/auto/net/Server.java
+++ src/main/java/com/cnty/auto/net/Server.java
@@ -1,10 +1,12 @@
 package com.cnty.auto.net;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 
 /**
@@ -16,21 +18,26 @@ import java.util.concurrent.Executors;
  * @version: X
  * Description:
  */
+@Slf4j
 public class Server {
     private ServerSocket serverSocket = null;
 
     /**
      * 线程池
+     * 弃用: private ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
      */
-    private ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+    private ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("pool-%d").build();
+    private ExecutorService threadPool = new ThreadPoolExecutor(5,200,0L,
+            TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>(1024),threadFactory,new ThreadPoolExecutor.AbortPolicy());
 
     /**
      * 构造方法
      */
-    Server() {
+    private Server() {
         try {
             int port = 8001;
             serverSocket = new ServerSocket(port);
+            log.info("服务器已启动..." + serverSocket);
             System.out.println("服务器已启动..." + serverSocket);
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,13 +48,15 @@ public class Server {
      * 启动，侦听客户端 v2afr
      */
     private void start() {
+        //noinspection InfiniteLoopStatement
         while (true) {
             Socket socket;
             try {
                 socket = serverSocket.accept();
+                log.info("新客户端: " + socket + "[已连接]");
                 System.out.println("有新的客户端连接进来," + socket);
                 // 将输入、输出交给线程处理
-                cachedThreadPool.execute(new ServerThread(socket));
+                threadPool.execute(new ServerThread(socket));
             } catch (IOException e) {
                 e.printStackTrace();
             }
